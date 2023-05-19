3150205c6901ade9124590a23cf1064377db79ee
Mustapha SLIMANI <itsmustaphahere@gmail.com>
2023-05-19 18:25:29 +0100

    Update README.md

--- src/main/java/ma/slimanimustapha/registrationservice/web/grpc/OwnerGrpcService.java
+++ src/main/java/ma/slimanimustapha/registrationservice/web/grpc/OwnerGrpcService.java
@@ -51,15 +51,34 @@ public class OwnerGrpcService extends OwnerServiceGrpc.OwnerServiceImplBase{
     }
 
     @Override
+    public void saveOwner(OwnerOuterClass.OwnerRequest request, StreamObserver<OwnerOuterClass.Owner> responseObserver) {
+        Owner owner = Owner.builder()
+                .id(null)
+                .email(request.getEmail())
+                .name(request.getName())
+                .birthDate(request.getBirthDate())
+                .build();
+        owner = ownerRepository.save(owner);
+        responseObserver.onNext(OwnerOuterClass.Owner.newBuilder()
+                .setId(owner.getId().intValue())
+                .setName(owner.getName())
+                .setEmail(owner.getEmail())
+                .setBirthDate(owner.getBirthDate())
+                .build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
     public void updateOwner(OwnerOuterClass.OwnerUpdateRequest request, StreamObserver<OwnerOuterClass.Owner> responseObserver) {
         Long ownerId = (long)request.getId();
         if(ownerRepository.existsById(ownerId)){
             System.out.println("Owner exists");
             Owner owner = ownerRepository.findById(ownerId).get();
-            if (request.getName().isEmpty()) owner.setName(request.getName());
-            if (request.getEmail().isEmpty()) owner.setEmail(request.getEmail());
-            if (request.getBirthDate().isEmpty()) owner.setBirthDate(request.getBirthDate());
-
+            owner.setName(request.getName());
+            owner.setEmail(request.getEmail());
+            owner.setBirthDate(request.getBirthDate());
+            System.out.println("Owner updated");
+            System.out.println(owner.getName());
             owner = ownerRepository.save(owner);
 
             OwnerOuterClass.Owner ownerResponse = OwnerOuterClass.Owner.newBuilder()
@@ -71,5 +90,110 @@ public class OwnerGrpcService extends OwnerServiceGrpc.OwnerServiceImplBase{
             responseObserver.onNext(ownerResponse);
             responseObserver.onCompleted();
         }
+    }
+
+    @Override
+    public void deleteOwner(OwnerOuterClass.OwnerId request, StreamObserver<OwnerOuterClass.Empty> responseObserver) {
+        Long ownerId = (long)request.getId();
+        if(ownerRepository.existsById(ownerId)){
+            ownerRepository.deleteById(ownerId);
+        }
+        responseObserver.onNext(OwnerOuterClass.Empty.newBuilder().build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getVehicules(OwnerOuterClass.Empty request, StreamObserver<OwnerOuterClass.VehiculeList> responseObserver) {
+        Iterable<Vehicle> vehicles = vehicleRepository.findAll();
+        OwnerOuterClass.VehiculeList.Builder responseBuilder = OwnerOuterClass.VehiculeList.newBuilder();
+        for (Vehicle vehicle : vehicles) {
+            OwnerOuterClass.Vehicule vehiculeResponse = OwnerOuterClass.Vehicule.newBuilder()
+                    .setId(vehicle.getId().intValue())
+                    .setRegNumber(vehicle.getRegNumber())
+                    .setBrand(vehicle.getBrand())
+                    .setFiscalPower(vehicle.getFiscalPower())
+                    .setModel(vehicle.getModel())
+                    .build();
+            responseBuilder.addVehicules(vehiculeResponse);
+        }
+        responseObserver.onNext(responseBuilder.build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getVehicule(OwnerOuterClass.VehicleId request, StreamObserver<OwnerOuterClass.Vehicule> responseObserver) {
+        Long vehicleId = (long)request.getId();
+        if(vehicleRepository.existsById(vehicleId)){
+            Vehicle vehicle = vehicleRepository.findById(vehicleId).orElse(null);
+            OwnerOuterClass.Vehicule vehiculeResponse = OwnerOuterClass.Vehicule.newBuilder()
+                    .setId(vehicle.getId().intValue())
+                    .setRegNumber(vehicle.getRegNumber())
+                    .setBrand(vehicle.getBrand())
+                    .setFiscalPower(vehicle.getFiscalPower())
+                    .setModel(vehicle.getModel())
+                    .setOwner(OwnerOuterClass.Owner.newBuilder()
+                            .setId(vehicle.getOwner().getId().intValue())
+                            .setName(vehicle.getOwner().getName())
+                            .setEmail(vehicle.getOwner().getEmail())
+                            .setBirthDate(vehicle.getOwner().getBirthDate())
+                            .build())
+                    .build();
+            responseObserver.onNext(vehiculeResponse);
+        }
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void saveVehicule(OwnerOuterClass.VehiculeRequest request, StreamObserver<OwnerOuterClass.Vehicule> responseObserver) {
+        Vehicle vehicle = Vehicle.builder()
+                .id(null)
+                .regNumber(request.getRegNumber())
+                .brand(request.getBrand())
+                .fiscalPower(request.getFiscalPower())
+                .model(request.getModel())
+                .build();
+        vehicle = vehicleRepository.save(vehicle);
+        OwnerOuterClass.Vehicule vehiculeResponse = OwnerOuterClass.Vehicule.newBuilder()
+                .setId(vehicle.getId().intValue())
+                .setRegNumber(vehicle.getRegNumber())
+                .setBrand(vehicle.getBrand())
+                .setFiscalPower(vehicle.getFiscalPower())
+                .setModel(vehicle.getModel())
+                .build();
+        responseObserver.onNext(vehiculeResponse);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void updateVehicule(OwnerOuterClass.VehiculeUpdateRequest request, StreamObserver<OwnerOuterClass.Vehicule> responseObserver) {
+        Long vehicleId = (long)request.getId();
+        if(vehicleRepository.existsById(vehicleId)){
+            Vehicle vehicle = vehicleRepository.findById(vehicleId).get();
+            vehicle.setRegNumber(request.getRegNumber());
+            vehicle.setBrand(request.getBrand());
+            vehicle.setFiscalPower(request.getFiscalPower());
+            vehicle.setModel(request.getModel());
+            vehicle = vehicleRepository.save(vehicle);
+
+            OwnerOuterClass.Vehicule vehiculeResponse = OwnerOuterClass.Vehicule.newBuilder()
+                    .setId(vehicle.getId().intValue())
+                    .setRegNumber(vehicle.getRegNumber())
+                    .setBrand(vehicle.getBrand())
+                    .setFiscalPower(vehicle.getFiscalPower())
+                    .setModel(vehicle.getModel())
+                    .build();
+            responseObserver.onNext(vehiculeResponse);
+            responseObserver.onCompleted();
+        }
+    }
+
+    @Override
+    public void deleteVehicule(OwnerOuterClass.VehicleId request, StreamObserver<OwnerOuterClass.Empty> responseObserver) {
+        Long vehicleId = (long)request.getId();
+        if(vehicleRepository.existsById(vehicleId)){
+            vehicleRepository.deleteById(vehicleId);
+        }
+        responseObserver.onNext(OwnerOuterClass.Empty.newBuilder().build());
+        responseObserver.onCompleted();
     }
 }
