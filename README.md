38a6380518fad13a0fd9b46e3b5122e58c174c8c
Mustapha SLIMANI <itsmustaphahere@gmail.com>
2023-05-19 16:28:09 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Registration Service
 ## Under development... 🚀
-
+### Architecture
+![Architecture](assets/Architecture.png)
 ### Class Diagram
 ![Class diagram](assets/Class%20diagram.png)
--- src/main/java/ma/slimanimustapha/registrationservice/web/grpc/OwnerGrpcService.java
+++ src/main/java/ma/slimanimustapha/registrationservice/web/grpc/OwnerGrpcService.java
@@ -3,6 +3,7 @@ package ma.slimanimustapha.registrationservice.web.grpc;
 import io.grpc.stub.StreamObserver;
 import lombok.AllArgsConstructor;
 import ma.slimanimustapha.registrationservice.entities.Owner;
+import ma.slimanimustapha.registrationservice.entities.Vehicle;
 import ma.slimanimustapha.registrationservice.repositories.OwnerRepository;
 import ma.slimanimustapha.registrationservice.repositories.VehicleRepository;
 import ma.slimanimustapha.registrationservice.web.grpc.stubs.OwnerOuterClass;
@@ -21,15 +22,54 @@ public class OwnerGrpcService extends OwnerServiceGrpc.OwnerServiceImplBase{
         Iterable<Owner> owners = ownerRepository.findAll();
         OwnerList.Builder responseBuilder = OwnerList.newBuilder();
         for (Owner owner : owners) {
-            OwnerOuterClass.Owner grpcOwner = OwnerOuterClass.Owner.newBuilder()
+            OwnerOuterClass.Owner ownerResponse = OwnerOuterClass.Owner.newBuilder()
                     .setId(owner.getId().intValue())
                     .setName(owner.getName())
                     .setEmail(owner.getEmail())
                     .setBirthDate(owner.getBirthDate())
                     .build();
-            responseBuilder.addOwners(grpcOwner);
+            responseBuilder.addOwners(ownerResponse);
         }
         responseObserver.onNext(responseBuilder.build());
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getOwner(OwnerOuterClass.OwnerId request, StreamObserver<OwnerOuterClass.Owner> responseObserver) {
+        Long ownerId = (long)request.getId();
+        if(ownerRepository.existsById(ownerId)){
+            Owner owner = ownerRepository.findById(ownerId).orElse(null);
+            OwnerOuterClass.Owner ownerResponse = OwnerOuterClass.Owner.newBuilder()
+                    .setId(owner.getId().intValue())
+                    .setName(owner.getName())
+                    .setEmail(owner.getEmail())
+                    .setBirthDate(owner.getBirthDate())
+                    .build();
+            responseObserver.onNext(ownerResponse);
+        }
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void updateOwner(OwnerOuterClass.OwnerUpdateRequest request, StreamObserver<OwnerOuterClass.Owner> responseObserver) {
+        Long ownerId = (long)request.getId();
+        if(ownerRepository.existsById(ownerId)){
+            System.out.println("Owner exists");
+            Owner owner = ownerRepository.findById(ownerId).get();
+            if (request.getName().isEmpty()) owner.setName(request.getName());
+            if (request.getEmail().isEmpty()) owner.setEmail(request.getEmail());
+            if (request.getBirthDate().isEmpty()) owner.setBirthDate(request.getBirthDate());
+
+            owner = ownerRepository.save(owner);
+
+            OwnerOuterClass.Owner ownerResponse = OwnerOuterClass.Owner.newBuilder()
+                    .setId(owner.getId().intValue())
+                    .setName(owner.getName())
+                    .setEmail(owner.getEmail())
+                    .setBirthDate(owner.getBirthDate())
+                    .build();
+            responseObserver.onNext(ownerResponse);
+            responseObserver.onCompleted();
+        }
     }
 }
